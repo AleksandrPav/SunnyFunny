29d3eaad98a401054cb110d31b598ec6679c3df7
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-07-27 22:38:33 +0300

    modal=done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 import swiper from "./swiper.js";
 import modalInit from "./modal.js";
-import modalFormsInit from "./forms_processing.js";
+// import modalFormsValidate from "./forms_processing.js";
 
 
 swiper.init();
 modalInit();
-// modalFormsInit();
+// modalFormsValidate();
 
 
 
